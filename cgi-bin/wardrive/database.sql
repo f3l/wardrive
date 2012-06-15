ea7d1558458c505d2f0a82013ee27db7c2de395f
Dominik Heidler <dheidler@gmail.com>
2012-06-15 16:01:44 +0200

    add sql comment

--- cgi-bin/wardrive/database.sql
+++ cgi-bin/wardrive/database.sql
@@ -24,6 +24,8 @@ delimiter //
 -- DROP PROCEDURE geodist;
 -- //
 
+-- the table-name could be set dynamicly like this, but it's faster having it static:
+-- http://stackoverflow.com/questions/6609778/mysql-store-procedure-dont-take-table-name-as-parameter
 -- returns the 10 nearest networks within /dist/ miles radius
 -- eg: CALL geodist(49.7152, 11.6472, 10, 1);
 CREATE PROCEDURE geodist(mylat float, mylon float, dist float, mylimit integer)
