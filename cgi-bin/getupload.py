03440f0071e10cd36ae199994ff69543459da4cf
Dominik Heidler <dheidler@gmail.com>
2012-06-25 17:22:26 +0200

    add upload ajax-query

---
+++ cgi-bin/getupload.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import cgi, os, sys
+import cgitb; cgitb.enable()
+from wardrive import Wardrive
+from wardrive.io_details import *
+import json
+
+wardrive = Wardrive('../wardrive.cfg')
+uploads = wardrive.uploads
+
+http_header = "Content-type: application/json\n"
+
+form = cgi.FieldStorage()
+
+mode = form.getvalue('mode', None)
+# comment(like) id list
+
+if not mode:
+	print "Content-type: text/plain\n"
+	print "Invalid mode"
+	sys.exit()
+
+uplist = []
+
+if mode == 'list':
+	# fixme: use limit, paging...
+
+	uplist_raw = uploads.get().sort({'id': 1}, reverse=True)
+
+	for upload in uplist_raw:
+		uplist.append({
+			'id': upload['id'],
+			'date': timestring(upload['timestamp']),
+			'uploader': upload['uploader'],
+			'filename': upload['filename'],
+			'comment': upload['comment']
+		})
+
+print http_header
+print json.dumps({'uploads': uplist})
